72725c2445c6aa8e76201955141d131c834f3cbb
fluks.github@gmail.com <you@example.com>
2025-08-07 19:33:29 +0300

    Check whether we should start on boot

--- app/src/main/java/com/jukkalaitinen/statusbarnetspeed/BootReceiver.java
+++ app/src/main/java/com/jukkalaitinen/statusbarnetspeed/BootReceiver.java
@@ -15,6 +15,9 @@ public class BootReceiver extends BroadcastReceiver {
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
             Intent serviceIntent = new Intent(context, NetSpeedService.class);
             SharedPreferences prefs = context.getSharedPreferences(Global.PREFS_NAME, MODE_PRIVATE);
+            if (!prefs.getBoolean(Global.PREF_ENABLED, false) || !prefs.getBoolean(Global.PREF_START_ON_BOOT, false))
+                return;
+
             serviceIntent.putExtra(Global.SERVICE_KEY_DELAY, prefs.getInt(Global.PREF_DELAY, Global.DEFAULT_DELAY));
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 context.startForegroundService(serviceIntent);
